e36b4b5ba6a9a27e52815176dc34a25a0e6ba29d
dfshfghj <2400011461@stu.pku.edu.cn>
2026-02-02 01:28:50 +0800

    feat: use tauri

---
+++ src-tauri/src/app/config.rs
@@ -0,0 +1,84 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WindowConfig {
+    pub url: String,
+    pub hide_title_bar: bool,
+    pub fullscreen: bool,
+    pub maximize: bool,
+    pub width: f64,
+    pub height: f64,
+    pub resizable: bool,
+    pub url_type: String,
+    pub always_on_top: bool,
+    pub dark_mode: bool,
+    pub disabled_web_shortcuts: bool,
+    pub activation_shortcut: String,
+    pub hide_on_close: bool,
+    pub incognito: bool,
+    pub title: Option<String>,
+    pub enable_wasm: bool,
+    pub enable_drag_drop: bool,
+    pub start_to_tray: bool,
+    #[serde(default)]
+    pub force_internal_navigation: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PlatformSpecific<T> {
+    pub macos: T,
+    pub linux: T,
+    pub windows: T,
+    pub android: T,
+    pub ios: T,
+}
+
+impl<T> PlatformSpecific<T> {
+    pub const fn get(&self) -> &T {
+        #[cfg(target_os = "macos")]
+        let platform = &self.macos;
+        #[cfg(target_os = "linux")]
+        let platform = &self.linux;
+        #[cfg(target_os = "windows")]
+        let platform = &self.windows;
+        #[cfg(target_os = "android")]
+        let platform = &self.android;
+        #[cfg(target_os = "ios")]
+        let platform = &self.ios;
+        
+        // Fallback for any other unknown platforms
+        #[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows", target_os = "android", target_os = "ios")))]
+        let platform = &self.linux;
+
+        platform
+    }
+}
+
+impl<T> PlatformSpecific<T>
+where
+    T: Copy,
+{
+    pub const fn copied(&self) -> T {
+        *self.get()
+    }
+}
+
+pub type UserAgent = PlatformSpecific<String>;
+pub type FunctionON = PlatformSpecific<bool>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PakeConfig {
+    pub windows: Vec<WindowConfig>,
+    pub user_agent: UserAgent,
+    pub system_tray: FunctionON,
+    pub system_tray_path: String,
+    pub proxy_url: String,
+    #[serde(default)]
+    pub multi_instance: bool,
+}
+
+impl PakeConfig {
+    pub fn show_system_tray(&self) -> bool {
+        self.system_tray.copied()
+    }
+}
---
+++ src-tauri/src/app/invoke.rs
@@ -0,0 +1,113 @@
+use crate::util::{check_file_or_append, get_download_message_with_lang, show_toast, MessageType};
+use std::fs::{self, File};
+use std::io::Write;
+use std::str::FromStr;
+use tauri::http::Method;
+use tauri::{command, AppHandle, Manager, Url, WebviewWindow};
+use tauri_plugin_http::reqwest::{ClientBuilder, Request};
+
+#[derive(serde::Deserialize)]
+pub struct DownloadFileParams {
+    url: String,
+    filename: String,
+    language: Option<String>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct BinaryDownloadParams {
+    filename: String,
+    binary: Vec<u8>,
+    language: Option<String>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct NotificationParams {
+    title: String,
+    body: String,
+    icon: String,
+}
+
+#[command]
+pub async fn download_file(app: AppHandle, params: DownloadFileParams) -> Result<(), String> {
+    let window: WebviewWindow = app.get_webview_window("pake").unwrap();
+    show_toast(
+        &window,
+        &get_download_message_with_lang(MessageType::Start, params.language.clone()),
+    );
+
+    let output_path = app.path().download_dir().unwrap().join(params.filename);
+    let file_path = check_file_or_append(output_path.to_str().unwrap());
+    let client = ClientBuilder::new().build().unwrap();
+
+    let response = client
+        .execute(Request::new(
+            Method::GET,
+            Url::from_str(&params.url).unwrap(),
+        ))
+        .await;
+
+    match response {
+        Ok(res) => {
+            let bytes = res.bytes().await.unwrap();
+
+            let mut file = File::create(file_path).unwrap();
+            file.write_all(&bytes).unwrap();
+            show_toast(
+                &window,
+                &get_download_message_with_lang(MessageType::Success, params.language.clone()),
+            );
+            Ok(())
+        }
+        Err(e) => {
+            show_toast(
+                &window,
+                &get_download_message_with_lang(MessageType::Failure, params.language),
+            );
+            Err(e.to_string())
+        }
+    }
+}
+
+#[command]
+pub async fn download_file_by_binary(
+    app: AppHandle,
+    params: BinaryDownloadParams,
+) -> Result<(), String> {
+    let window: WebviewWindow = app.get_webview_window("pake").unwrap();
+    show_toast(
+        &window,
+        &get_download_message_with_lang(MessageType::Start, params.language.clone()),
+    );
+    let output_path = app.path().download_dir().unwrap().join(params.filename);
+    let file_path = check_file_or_append(output_path.to_str().unwrap());
+    let download_file_result = fs::write(file_path, &params.binary);
+    match download_file_result {
+        Ok(_) => {
+            show_toast(
+                &window,
+                &get_download_message_with_lang(MessageType::Success, params.language.clone()),
+            );
+            Ok(())
+        }
+        Err(e) => {
+            show_toast(
+                &window,
+                &get_download_message_with_lang(MessageType::Failure, params.language),
+            );
+            Err(e.to_string())
+        }
+    }
+}
+
+#[command]
+pub fn send_notification(app: AppHandle, params: NotificationParams) -> Result<(), String> {
+    use tauri_plugin_notification::NotificationExt;
+    app.notification()
+        .builder()
+        .title(&params.title)
+        .body(&params.body)
+        .icon(&params.icon)
+        .show()
+        .unwrap();
+    Ok(())
+}
